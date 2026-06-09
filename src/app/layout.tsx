b3747c412fedbf7949e82aa667c745cbc00fe980
Leon22-r <njihialeon00@gmail.com>
2026-06-09 19:04:27 +0300

    mobile bug

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,7 +17,7 @@ export const metadata: Metadata = {
 };
 
 export const viewport: Viewport = {
-  themeColor: "#101828",
+  themeColor: "#060711",
   width: "device-width",
   initialScale: 1,
   viewportFit: "cover",
